502fcbb6dc86732432170010751476e3267fed2a
yahyazouhri <49101753+yahyazouhri@users.noreply.github.com>
2019-07-17 22:43:10 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,33 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "+";
+var adminprefix = '#'
+
+
+const developers = ["462936292636884994"]
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'setg')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+     if (message.content === (adminprefix + "leave")) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith(adminprefix + 'setw')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'setl')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'sets')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/BlmShop");
+      message.channel.send(`**✅**`)
+});
+
+
+client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
